70d493988da4bd0482849b2e2cefc12f41f610f3
davidlaprade <david.laprade@gmail.com>
2014-08-03 22:44:05 -0400

    defined variables, min from each row subtracted from each cell

--- hungarian.rb
+++ hungarian.rb
@@ -6,14 +6,35 @@
 # for array reference: http://www.ruby-doc.org/core-2.1.2/Array.html
 
 def hungarian
-	# going to use an array, not a matrix so that values can be added/removed and accessed
+	# going to use an array so that values can be added/removed and accessed
 	ORIGINAL_MATRIX = self
+	# makes a copy of the self array
 	WORKING_MATRIX = Array.new(self)
 
+	# ensure self array has same number of elements in each row...
+	# ensure self array has only integers in each row, and that each row contains each integer...
+
 	# want to create an empty array the same size as the original
-	rows = ORIGINAL_MATRIX[0].row_length
-	columns = ORIGINAL_MATRIX.column_height
-	ASSIGNING_MATRIX = Array.new(columns) { Array.new(rows) }
+	columns = ORIGINAL_MATRIX.number_columns
+	rows = ORIGINAL_MATRIX.number_rows
+	ASSIGNING_MATRIX = Array.new(rows) { Array.new(columns) }
+
+	# for now, make these pre-set; later have them input by user
+	# ceil rounds a float up to the nearest integer
+	max_row_assignment = 1
+	min_row_assignment = 1
+	max_col_assignment = (rows/columns).ceil
+	min_col_assignment = 1
+
+	# subtracts the lowest value in each row from each member of that row
+	WORKING_MATRIX.each do |row|
+		min = row.min
+		row.each do |cell|
+			cell = cell - min
+		end
+	end
+	Doesn't work in IRB for some reason!
+
 
 
 
@@ -22,11 +43,11 @@ end
 
 
 # Helper methods
-def row_length
+def number_columns
 	self[0].length
 end
 
-def column_height
+def number_rows
 	self.length
 end
 
