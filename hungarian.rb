256cf97e507a7ba71a16e5746cbe3c08d8300664
davidlaprade <david.laprade@gmail.com>
2014-08-03 20:42:34 -0400

    changed matrices to arrays

--- hungarian.rb
+++ hungarian.rb
@@ -3,16 +3,31 @@
 # solution_array is a set of ordered pairs (n, m) such that n is the number of a row in matrix_to_solve and m is the assignment in that row
 # solution_array.measure will add all of the values of the assignments in the solution
 
+# for array reference: http://www.ruby-doc.org/core-2.1.2/Array.html
 
 def hungarian
-	ORIGINAL_MATRIX = self.clone
-	# want to create a new matrix the same size as the old one and fill it with zeros
-	# http://stackoverflow.com/questions/15884376/ruby-2-0-how-to-access-element-in-matrix-by-coordinate
-	ASSIGNING_MATRIX = Matrix.build(rows, columns) {0}
+	# going to use an array, not a matrix so that values can be added/removed and accessed
+	ORIGINAL_MATRIX = self
+	WORKING_MATRIX = Array.new(self)
+
+	# want to create an empty array the same size as the original
+	rows = ORIGINAL_MATRIX[0].row_length
+	columns = ORIGINAL_MATRIX.column_height
+	ASSIGNING_MATRIX = Array.new(columns) { Array.new(rows) }
 
 
 
 
+end
+
+
+# Helper methods
+def row_length
+	self[0].length
+end
+
+def column_height
+	self.length
 end
 
 def measure
