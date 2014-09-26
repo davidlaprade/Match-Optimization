580920cc18a17616487e35014d4e9d9d2e7feffc
davidlaprade <david.laprade@gmail.com>
2014-09-26 16:30:30 -0400

    found examples that make the finish_assignment method go into an endless loop

--- testing_spec.rb
+++ testing_spec.rb
@@ -41,26 +41,35 @@ describe Array, ".finish_assignment" do
 		expect(matrix.solution?).to eq(true)
 	end
 
-	40.times do
-		it "yeilds produces a solved mask array when called on an array of random size filled with random values" do
-			cols = rand(15)+1
-			rows = rand(15)+1
-			matrix = Array.new(rows) {Array.new(cols) {rand(9)+1}}
-			original = matrix.dup
-			make_matrix_solveable(matrix)
-			assign_needy_zeros(matrix).finish_assignment
-			solution = matrix.solution?
-			original.print_readable if solution != true
-			expect(solution).to eq(true)
-		end	
+	it "does not get stuck in an infinite loop" do
+		matrix = [[2, 9, 6, 1, 3, 4, 6],
+			[2, 3, 4, 7, 8, 3, 8],
+			[6, 3, 1, 6, 6, 4, 6],
+			[4, 1, 2, 7, 2, 6, 6],
+			[7, 5, 7, 1, 9, 4, 3],
+			[7, 3, 1, 4, 7, 5, 4],
+			[3, 2, 3, 1, 3, 4, 1],
+			[2, 2, 2, 4, 4, 4, 2]]
+		make_matrix_solveable(matrix)
+		assign_needy_zeros(matrix).finish_assignment
+		expect(matrix.solution?).to eq(true)
 	end
 
-end
+	it "does not get stuck in an infinite loop" do
+		matrix = [[5, 2, 2, 3, 1, 8, 5, 9, 5],
+			[5, 4, 4, 4, 1, 7, 6, 2, 9],
+			[6, 7, 7, 7, 5, 9, 5, 7, 8],
+			[4, 8, 7, 1, 7, 4, 9, 9, 1],
+			[2, 5, 4, 6, 4, 9, 1, 4, 2],
+			[6, 7, 1, 7, 6, 1, 9, 9, 2],
+			[7, 5, 7, 2, 6, 5, 1, 2, 7],
+			[7, 5, 9, 8, 7, 7, 4, 5, 7]]
+		make_matrix_solveable(matrix)
+		assign_needy_zeros(matrix).finish_assignment
+		expect(matrix.solution?).to eq(true)
+	end
 
-
-describe "make_matrix_solveable" do
-
-	it "should finish running on problematic array" do
+	it "does not get stuck in an infinite loop" do
 		matrix = [[9, 1, 1, 2, 5, 2, 9, 9, 5, 2, 1, 6, 3],
 			[9, 8, 9, 7, 8, 8, 2, 1, 2, 9, 6, 2, 3],
 			[7, 5, 8, 4, 8, 8, 9, 9, 5, 6, 1, 8, 9],
@@ -76,9 +85,30 @@ describe "make_matrix_solveable" do
 			[2, 4, 8, 7, 8, 5, 2, 9, 2, 8, 4, 8, 6],
 			[1, 2, 7, 2, 3, 8, 7, 9, 2, 6, 5, 5, 7],
 			[8, 1, 1, 5, 4, 8, 8, 1, 8, 6, 5, 9, 7]]
-		expect(make_matrix_solveable(matrix).solveable?).to eq("true")
+		make_matrix_solveable(matrix)
+		assign_needy_zeros(matrix).finish_assignment
+		expect(matrix.solution?).to eq(true)
 	end
 
+
+	# 40.times do
+	# 	it "yeilds produces a solved mask array when called on an array of random size filled with random values" do
+	# 		cols = rand(15)+1
+	# 		rows = rand(15)+1
+	# 		matrix = Array.new(rows) {Array.new(cols) {rand(9)+1}}
+	# 		original = matrix.dup
+	# 		make_matrix_solveable(matrix)
+	# 		assign_needy_zeros(matrix).finish_assignment
+	# 		solution = matrix.solution?
+	# 		original.print_readable if solution != true
+	# 		expect(solution).to eq(true)
+	# 	end	
+	# end
+
+end
+
+
+describe "make_matrix_solveable" do
 
 	it "yeilds a solveable array when called on an uneven array" do
 		matrix = [[6, 2, 2, 6, 4, 7, 3],[2, 6, 6, 6, 9, 2, 5],[6, 2, 4, 1, 2, 3, 1],[1, 5, 6, 5, 8, 2, 5],
@@ -135,7 +165,19 @@ describe "make_matrix_solveable" do
 		expect(make_matrix_solveable(matrix).solveable?).to eq("true")
 	end
 
-	
+	it "should not throw a RuntimeError" do
+		matrix = [[4, 3, 1, 7, 3],
+			[6, 8, 9, 8, 9],
+			[4, 1, 3, 2, 4],
+			[3, 4, 6, 2, 9],
+			[1, 2, 9, 4, 4],
+			[2, 4, 2, 5, 2],
+			[3, 9, 8, 3, 7],
+			[6, 3, 5, 5, 5],
+			[2, 8, 7, 3, 8]]
+		expect(make_matrix_solveable(matrix).solveable?).to eq("true")
+	end
+
 	it "should not throw a RuntimeError" do
 		matrix = [[4, 3, 7, 9, 4, 6],[6, 0, 2, 6, 1, 5],[1, 8, 6, 9, 5, 7],[6, 2, 5, 6, 2, 4],[9, 0, 6, 3, 0, 7],[2, 4, 9, 7, 3, 5],
 	[8, 1, 0, 5, 8, 5],[8, 9, 4, 6, 4, 6],[9, 3, 4, 1, 7, 0],[7, 1, 9, 9, 9, 6],[4, 8, 1, 1, 9, 0],[0, 0, 1, 2, 0, 7]]
